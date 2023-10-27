243e383ab34d1a9e6d5d0c2c090c322b6fcdae72
unBenchtheBench <joosep.tamm.001@gmail.com>
2023-10-27 13:45:31 +0300

    Player creation
    Made basic moving player, made tileset properly pixel art in unity settings.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+  
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * 0.01f, Input.GetAxis("Vertical") * 0.01f, 0);
     }
 }
