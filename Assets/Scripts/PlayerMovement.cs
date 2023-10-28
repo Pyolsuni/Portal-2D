bec11b44eb8e1793887dde4ddc4bb521679a50ab
unBenchtheBench <joosep.tamm.001@gmail.com>
2023-10-28 18:15:11 +0300

    Player character and collisions with level walls
    Added the player character and its movement script, additionally made player correctly collide with level walls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D rb;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * 5;
     }
 }
