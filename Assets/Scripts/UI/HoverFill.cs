cd68f975cc86effafc0eb53445efd6ed61354915
SanRoosalu <roosalu.sander@gmail.com>
2023-11-24 19:37:25 +0200

    Added Main Menu Scene; Fixed typos with canvases
    No functionality in Main Menu Scene yet

---
+++ Assets/Scripts/UI/HoverFill.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoverFill : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public Image image;
+    private bool inBounds;
+
+    private void Update() {
+        if (inBounds) { image.fillAmount += 0.04f; }
+        else { image.fillAmount -= 0.04f; }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        inBounds = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        inBounds = false;
+    }
+}
