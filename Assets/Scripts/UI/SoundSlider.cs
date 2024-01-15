57fd2b60b9c081dd450bda15a2b6e78c363e600e
SanRoosalu <roosalu.sander@gmail.com>
2024-01-15 20:04:58 +0200

    Added pause menu to all 5 levels

--- Assets/Scripts/UI/SoundSlider.cs
+++ Assets/Scripts/UI/SoundSlider.cs
@@ -13,20 +13,13 @@ public class SoundSlider : MonoBehaviour
     void Start()
     {
         sounds = FindObjectsOfType<AudioSource>();
-
         sliderText.text = soundSlider.value.ToString("0.00");
+
         soundSlider.onValueChanged.AddListener((value) => {
             sliderText.text = value.ToString("0.00");
-
             foreach (AudioSource source in sounds) {
                 source.volume = Mathf.Log10(value);
             }
         });
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
